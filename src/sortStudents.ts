bf1265811bfa21fbaae4c5f61250df2aae50ed75
CCyrillus <kyrill.kiril@gmail.com>
2022-01-31 00:43:29 +0200

    final solution

--- src/sortStudents.ts
+++ src/sortStudents.ts
@@ -1,16 +1,77 @@
 
 export interface Student {
-  // describe Student interface
+  name: string,
+  surname: string,
+  age: number,
+  married: boolean,
+  grades: number[],
 }
 
 export enum SortType {
-  // describe SortType enum
+  Name,
+  Surname,
+  Age,
+  Married,
+  AverageGrade
 }
 
-// create SortOrder type
-export type SortOrder;
+export type SortOrder = 'asc' | 'desc';
 
+export function sortStudents(
+  students: Student[],
+  sortBy: SortType,
+  order: SortOrder = 'asc',
+): Student[] {
+  const copy = [...students];
 
-export function sortStudents(students, sortBy, order) {
-  // write your function
+  type SortingCallback = (x: Student, y: Student) => number;
+
+  const makeSortingCallback = (
+    type: SortType,
+    sortOrder: SortOrder,
+  ): SortingCallback => {
+    const isAscendingOrder = sortOrder === 'asc';
+
+    switch (sortBy) {
+      case SortType.Age:
+        return isAscendingOrder
+          ? (x, y): number => x.age - y.age
+          : (x, y): number => y.age - x.age;
+
+      case SortType.Surname:
+        return isAscendingOrder
+          ? (x, y): number => x.surname.localeCompare(y.surname)
+          : (x, y): number => y.surname.localeCompare(x.surname);
+
+      case SortType.Married:
+        return isAscendingOrder
+          ? (x, y): number => +x.married - +y.married
+          : (x, y): number => +y.married - +x.married;
+
+      case SortType.AverageGrade:
+        return (x, y): number => {
+          const getTotalGrade = (total: number, current: number): number => {
+            return total + current;
+          };
+
+          const currentTotal = x.grades.reduce(getTotalGrade, 0)
+
+            / x.grades.length;
+
+          const nextTotal = y.grades.reduce(getTotalGrade, 0)
+            / y.grades.length;
+
+          return isAscendingOrder
+            ? currentTotal - nextTotal
+            : nextTotal - currentTotal;
+        };
+
+      default:
+        return isAscendingOrder
+          ? (x, y): number => x.name.localeCompare(y.name)
+          : (x, y): number => y.name.localeCompare(x.name);
+    }
+  };
+
+  return copy.sort(makeSortingCallback(sortBy, order));
 }
